9ef0658830afbae42d188bfc40c33c8103ee2290
cristianepagine <cristianepagine@gmail.com>
2023-10-07 21:20:51 -0300

    service

---
+++ Services/src/app/components/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
+export class CardComponent implements OnInit {
+  name: string = 'PIKACHU';
+  attributesTypes: string[] = ['FIRE', 'ROCK'];
+  constructor(private service: PokemonService) {}
+  ngOnInit(): void {
+
+  }
+}
